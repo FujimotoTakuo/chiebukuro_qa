353c8da5c9c8cad68d3e1138c3d4a9e7f326cc5b
FujimotoTakuo <takuo-fujimoto@to-be.co.jp>
2016-08-25 17:54:46 +0900

    das
    das

--- setup_file.sh
+++ setup_file.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+curDir = `pwd`
+shDir = `dirname $0`
+cd $shDir
+
+cat origin/question* > chie_question.txt
+cat origin/answer* > chie_answer.txt
+cat goo/question* > goo_question.txt
+cat goo/answer* > goo_answer.txt
+cat ok/question* > ok_question.txt
+cat ok/answer* > ok_answer.txt
